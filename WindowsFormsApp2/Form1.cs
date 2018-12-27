af745c3ffeaf9fc2d64fb42e8dfc4b8c17c3c658
Dimitros617 <frolik.dominik@gmail.com>
2018-12-27 20:09:32 +0100

    Přidejte soubory projektu.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int s = Screen.PrimaryScreen.Bounds.Width;
+            int v = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+            this.Size = new Size(s, v);
+
+        }
+    }
+}
