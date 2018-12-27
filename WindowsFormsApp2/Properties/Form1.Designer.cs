e0670306a51a8bb29d74194c9be82a0c0acba446
Dimitros617 <frolik.dominik@gmail.com>
2018-12-27 21:07:36 +0100

    Black background s nolovým časovačem na místě

---
+++ WindowsFormsApp2/Properties/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp2.Properties
+{
+    partial class Main_UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cas
+            // 
+            this.cas.AutoSize = true;
+            this.cas.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cas.ForeColor = System.Drawing.Color.White;
+            this.cas.Location = new System.Drawing.Point(12, 23);
+            this.cas.Name = "cas";
+            this.cas.Size = new System.Drawing.Size(290, 112);
+            this.cas.TabIndex = 1;
+            this.cas.Text = "00:00";
+            this.cas.Click += new System.EventHandler(this.cas_Click);
+            // 
+            // Main_UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main_UI";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_UI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label cas;
+    }
+}
--- WindowsFormsApp2/Properties/Form1.cs
+++ WindowsFormsApp2/Properties/Form1.cs
@@ -8,21 +8,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp2
+namespace WindowsFormsApp2.Properties
 {
-    public partial class Form1 : Form
+    public partial class Main_UI : Form
     {
-        public Form1()
+        public Main_UI()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Main_UI_Load(object sender, EventArgs e)
         {
             int s = Screen.PrimaryScreen.Bounds.Width;
             int v = Screen.PrimaryScreen.Bounds.Height;
+
             this.Location = new Point(0, 0);
             this.Size = new Size(s, v);
+            
+        }
+
+        private void cas_Click(object sender, EventArgs e)
+        {
 
         }
     }
