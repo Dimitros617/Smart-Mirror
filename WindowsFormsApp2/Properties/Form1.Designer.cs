0ba9d35f03f0070fa727dc50c530391592e979c6
Dimitros617 <frolik.dominik@gmail.com>
2018-12-28 16:03:48 +0100

    Přidané dynamické notifikace a check internet connection

--- WindowsFormsApp2/Properties/Form1.Designer.cs
+++ WindowsFormsApp2/Properties/Form1.Designer.cs
@@ -32,14 +32,20 @@
             this.cas = new System.Windows.Forms.Label();
             this.cas_sec = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.OnlineStatus = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NotifLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.OnlineStatus)).BeginInit();
             this.SuspendLayout();
             // 
             // cas
             // 
             this.cas.AutoSize = true;
+            this.cas.BackColor = System.Drawing.Color.Transparent;
             this.cas.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.cas.ForeColor = System.Drawing.Color.White;
             this.cas.Location = new System.Drawing.Point(12, 23);
+            this.cas.Margin = new System.Windows.Forms.Padding(0);
             this.cas.Name = "cas";
             this.cas.Size = new System.Drawing.Size(290, 112);
             this.cas.TabIndex = 1;
@@ -49,6 +55,7 @@
             // cas_sec
             // 
             this.cas_sec.AutoSize = true;
+            this.cas_sec.BackColor = System.Drawing.Color.Transparent;
             this.cas_sec.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.cas_sec.ForeColor = System.Drawing.Color.White;
             this.cas_sec.Location = new System.Drawing.Point(271, 68);
@@ -59,7 +66,40 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // OnlineStatus
+            // 
+            this.OnlineStatus.BackColor = System.Drawing.Color.Black;
+            this.OnlineStatus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.OnlineStatus.Location = new System.Drawing.Point(692, 348);
+            this.OnlineStatus.Name = "OnlineStatus";
+            this.OnlineStatus.Size = new System.Drawing.Size(96, 90);
+            this.OnlineStatus.TabIndex = 3;
+            this.OnlineStatus.TabStop = false;
+            this.OnlineStatus.Click += new System.EventHandler(this.OnlineStatus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // NotifLabel
+            // 
+            this.NotifLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.NotifLabel.AutoSize = true;
+            this.NotifLabel.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.NotifLabel.ForeColor = System.Drawing.Color.White;
+            this.NotifLabel.Location = new System.Drawing.Point(355, 9);
+            this.NotifLabel.Name = "NotifLabel";
+            this.NotifLabel.Size = new System.Drawing.Size(0, 17);
+            this.NotifLabel.TabIndex = 5;
+            this.NotifLabel.Click += new System.EventHandler(this.NotifLabel_Click);
             // 
             // Main_UI
             // 
@@ -67,12 +107,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NotifLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OnlineStatus);
             this.Controls.Add(this.cas_sec);
             this.Controls.Add(this.cas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Main_UI";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Main_UI_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Main_UI_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.OnlineStatus)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,5 +127,8 @@
         private System.Windows.Forms.Label cas;
         private System.Windows.Forms.Label cas_sec;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox OnlineStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NotifLabel;
     }
 }
--- WindowsFormsApp2/Properties/Form1.cs
+++ WindowsFormsApp2/Properties/Form1.cs
@@ -10,18 +10,49 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
 
 namespace WindowsFormsApp2.Properties
 {
     public partial class Main_UI : Form
     {
+
+        private int timer;
+        private int NotifCounter;
+        LinkedList<Label> Notifikace;
+
+
         public Main_UI()
         {
             InitializeComponent();
+            Notifikace = new LinkedList<Label>();
+        }
+
+        private void Main_UI_Paint(object sender, PaintEventArgs e)
+        {
+
+            Form1_Paint(sender, e);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e) {
+
+            Pen pen = new Pen(Color.White, 5);
+
+            int yposun = 110;
+            int xposun = 400;
+            Point p1 = new Point(cas.Location.X, cas.Location.Y + yposun);
+            Point p2 = new Point(cas.Location.X + xposun, cas.Location.Y + yposun);
+
+            e.Graphics.DrawLine(pen, p1, p2);
         }
 
         private void Main_UI_Load(object sender, EventArgs e)
         {
+
+            
+
             int s = Screen.PrimaryScreen.Bounds.Width;
             int v = Screen.PrimaryScreen.Bounds.Height;
 
@@ -32,25 +63,51 @@ namespace WindowsFormsApp2.Properties
 
             //-------------- set cas
 
-            string time = String.Format("{0:00}", DateTime.Now.Hour) + ":" + String.Format("{0:00}", DateTime.Now.Minute); ;
-
-            cas.Text = time;
+            cas.Text = String.Format("{0:00}", DateTime.Now.Hour) + ":" + String.Format("{0:00}", DateTime.Now.Minute); ;
             cas_sec.Text = string.Format("{0:00}", DateTime.Now.Second);
 
-            //--------------
+            //-------------- set pic internet connection
+
+            Image image;
+
+            try
+            {
+                if (CheckForInternetConnection())
+                {
+                      image = Image.FromFile(@"..\\Image\\online.png");
+                   // image = Image.FromFile("online.png");
+                }
+                else
+                {
+                    image = Image.FromFile(@"..\\Image\\offline.png");
+                }
+                OnlineStatus.Image = image;
+            }
+            catch {
+
+                Console.WriteLine("nebylo možno nalést obrázek pripojení k internetu");
+                Notify("nebylo možno nalést obrázek pripojení k internetu");
+            }
+
+            
+
+            
 
             Console.WriteLine("-------------NECO---------------");
             Console.WriteLine();
             Console.WriteLine(CheckForInternetConnection());
 
             timer1.Start();
-            cas.Refresh();
+            this.Refresh();
         }
 
         private void cas_Click(object sender, EventArgs e){
+
+            this.Close();
+
         }
 
-        public static bool CheckForInternetConnection()
+        private bool CheckForInternetConnection()
         {
             try
             {
@@ -73,6 +130,67 @@ namespace WindowsFormsApp2.Properties
         {
             cas_sec.Text = string.Format("{0:00}", DateTime.Now.Second);
             cas_sec.Refresh();
+
+            cas.Text = String.Format("{0:00}", DateTime.Now.Hour) + ":" + String.Format("{0:00}", DateTime.Now.Minute); ;
+            cas.Refresh();
+
+
+            if (Notifikace.Count != 0)
+
+                try
+                {
+                    foreach (Label label in Notifikace)
+                    {
+
+                        if (label.TabIndex == 0)
+                        {
+                            if (label.Location.Y > -20)
+                                label.Location = new Point(label.Location.X, label.Location.Y - 1);
+                            else
+                                Notifikace.Remove(label);
+                        }
+                        else
+                        {
+
+                            label.TabIndex--;
+
+                        }
+
+                    }
+                }
+                catch {
+                   // Notify("Chyba při notifikaci");
+                }
         }
+
+        private void OnlineStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NotifLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Notify(String s) {
+
+            int locx = Screen.PrimaryScreen.Bounds.Width/2;
+            int locy = Notifikace.Count == 0 ? 20 : Notifikace.ElementAt(0).Location.Y + 20;
+
+            Label label = new Label();
+            label.Location = new System.Drawing.Point(locx, locy);
+            label.Name = "label";
+            label.Text = s;
+            label.AutoSize = true;
+            label.ForeColor = System.Drawing.Color.White;
+            label.TabIndex = 300;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            this.Controls.Add(label as Control);
+            Notifikace.AddFirst(label);
+
+        }
+
+
     }
 }
